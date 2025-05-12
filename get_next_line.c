86112d60f36ed6984e96b570ca8b9354323cae53
Gustavo Magno Oliveira Calmon De Siqueira <gustoliv@c1r1s6.42lisboa.com>
2025-05-12 18:07:11 +0100

    bora

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gustoliv <gustoliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 17:15:15 by gustoliv          #+#    #+#             */
-/*   Updated: 2025/05/11 16:24:35 by gustoliv         ###   ########.fr       */
+/*   Updated: 2025/05/12 16:59:48 by gustoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,42 @@ char	*get_next_line(int fd)
 {
 	static char	s[BUFFER_SIZE + 1];
 	char		*line;
+	int			readbytes = 0;
 
 	line = NULL;
-	if (s[0] != '\0')
-	{
-		line = ft_strjoin(line, s);	
+	// if (s[0] != '\0')
+	// {
+	// 	line = ft_strjoin(line, s);
+	// }
+	while (*s || read(fd, s, BUFFER_SIZE) > 0)
+	{	
+		line = ft_strjoin(line, s, line);
+		if (!check_newline(s))
+			return (clean_buffer(s), line);
+		clean_buffer(s);
 	}
-	while (read(fd, s, BUFFER_SIZE) > 0 && check_newline(s))
-		line = ft_strjoin(line, s);
-	if (!check_newline(s))
-		line = ft_strjoin(line, s);
-	clean_buffer(s);
+	// if (!check_newline(s))
+	// 	line = ft_strjoin(line, s);
+	
+	// printf("LIMPANDO : %s\n", s);
 	return (line);
 }
 int main()
 {
 	char	*s;
 	int fd = open("test.txt", O_RDONLY);
+	// s = get_next_line(fd);
+	//printf("%s", s);
+	
 	s = get_next_line(fd);
-
 	printf("%s", s);
+	free(s);
+	
 	s = get_next_line(fd);
-
 	printf("%s", s);
+	free(s);
+	
 	s = get_next_line(fd);
-
 	printf("%s", s);
+	free(s);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: gustoliv <gustoliv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/05 17:09:08 by gustoliv          #+#    #+#             */
-/*   Updated: 2025/05/11 17:34:30 by gustoliv         ###   ########.fr       */
+/*   Updated: 2025/05/12 16:56:32 by gustoliv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10
+#  define BUFFER_SIZE 20
 # endif
 
 # include <limits.h>
@@ -23,7 +23,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
-char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strjoin(char *s1, char *s2, char *s1_temp);
 int		check_newline(char *str);
 void	clean_buffer(char *buffer);
 
